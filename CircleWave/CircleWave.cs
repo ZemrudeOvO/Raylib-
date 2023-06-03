ed2777afadca38cd1acfa364ac19d2b0c7da2469
Zemrude <eufonio@qq.com>
2023-06-03 19:24:46 +0800

    CircleWave & PerlinWave

---
+++ CircleWave/CircleWave.cs
@@ -0,0 +1,34 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace AAA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int ScreenSize = 1000;
+            int section = 16;
+            Raylib.InitWindow(ScreenSize, ScreenSize, "Perlin");
+            Raylib.SetTargetFPS(50);
+
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLANK);
+
+
+                for (int i = 0; i < section; i++)
+                {
+                    int width = ScreenSize / 2 / (section + 1) + (i * ScreenSize / 2 / (section + 1));
+                    float deltaTime = 0.25f;
+                    float deltaWidth = ScreenSize / 2 / (section + 2) / 2 * MathF.Sin((float)Raylib.GetTime() + i * deltaTime);
+                    Raylib.DrawRing(new Vector2(ScreenSize / 2, ScreenSize / 2), width - deltaWidth, width + deltaWidth, 0, 360, 360, Color.WHITE);
+                }
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
---
+++ PerlinWave/PerlinWave.cs
@@ -0,0 +1,40 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace AAA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int ScreenSize = 1000;
+            int frameCounter = 0;
+            int lineNum = 75;
+            int noiseSize = 500;
+            int yOffset = 125;
+            Raylib.InitWindow(ScreenSize, ScreenSize, "Perlin");
+            Raylib.SetTargetFPS(50);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLANK);
+
+                frameCounter++;
+                Image noise = Raylib.GenImagePerlinNoise(noiseSize, noiseSize, 0, frameCounter, 1);
+                for (int height = 0; height < lineNum; height++)
+                {
+
+                    Vector2[] vec = new Vector2[noiseSize];
+                    for (int width = 0; width < noiseSize; width++)
+                    {
+                        vec[width] = new Vector2(width * Raylib.GetScreenWidth() / noiseSize, Raylib.GetImageColor(noise, width, height * noiseSize / lineNum).r + height * Raylib.GetScreenHeight() / lineNum - yOffset);
+                    }
+                    Raylib.DrawLineStrip(vec, noiseSize, Color.WHITE);
+                }
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
