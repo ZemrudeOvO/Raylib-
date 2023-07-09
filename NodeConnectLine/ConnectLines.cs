17f99295d3069793a26c3bf6e326890e0b418ee8
Zemrude <eufonio@qq.com>
2023-07-09 22:14:06 +0800

    no message

---
+++ NodeConnectLine/ConnectLines.cs
@@ -0,0 +1,23 @@
+﻿//node class
+//connect class & method
+using Raylib_cs;
+using System.Numerics;
+
+public class Lines
+{ 
+    public static void Main()
+    {
+        Raylib.InitWindow(640, 360, "Lines");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLANK);
+            Raylib.DrawLineEx(Raylib.GetMousePosition(), new Vector2((550 - Raylib.GetMouseX()) / 2 + Raylib.GetMouseX(), Raylib.GetMouseY()), 5, Color.WHITE);
+            Raylib.DrawRectangleGradientH(Raylib.GetMouseX() - 50, Raylib.GetMouseY() - 50, 100, 100, Color.GOLD, Color.SKYBLUE);
+            Raylib.DrawRectangleGradientH(500, 250, 100, 100, Color.GOLD, Color.SKYBLUE);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
---
+++ Timer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Raylib_cs;
+using System;
+using System.Timers;
+
+class TimeClock
+{
+    private static System.Timers.Timer? aTimer;
+    static DateTime date;
+
+    public static void Main()
+    {
+        Raylib.InitWindow(640, 360, "Timer");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+            { 
+                SetTimer();
+                Console.WriteLine(date.Second);
+	        }
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_ESCAPE))
+            {
+                aTimer?.Stop();
+                aTimer?.Dispose();
+	        }
+
+            Raylib.DrawText((date.Second + date.Minute * 60).ToString(), 0, 0, 30, Color.WHITE);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+
+    static void SetTimer()
+    {
+        aTimer = new System.Timers.Timer(2000);
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private static void OnTimedEvent(object? sender, ElapsedEventArgs e)
+    {
+        date = e.SignalTime;
+    }
+}
